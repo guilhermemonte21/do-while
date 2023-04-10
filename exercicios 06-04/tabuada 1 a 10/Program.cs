802257fbb81281821b5160264a1c73953aa78b0f
Guilherme Pereira Monte <6352573355@info.local>
2023-04-10 16:16:01 -0300

    projeto

---
+++ exercicios 06-04/tabuada 1 a 10/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine($"Ola, como vai seu dia");
+string saudacoes = Console.ReadLine();
+
+for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+{
+    for( int multiplicado = 1; multiplicado <= 10;multiplicado++){
+        Console.WriteLine($"{multiplicado} x {multiplicador} = {multiplicado*multiplicador}");
+        
+    }
+    
+}
+
